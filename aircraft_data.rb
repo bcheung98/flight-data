d5372db6a2fcb97bf50c5f088c68468e31850668
Brian <briantcheung@gmail.com>
2021-05-12 15:21:42 -0700

    Added aircraft data

---
+++ aircraft_data.rb
@@ -0,0 +1,110 @@
+aircraft_data = {
+    "Airbus A220-100" => {
+        max_capacity: 108,
+        seat_arrangement: "2-3"
+    },
+    "Airbus A220-300" => {
+        max_capacity: 160,
+        seat_arrangement: "2-3"
+    },
+    "Airbus A319" => {
+        max_capacity: 132,
+        seat_arrangement: "3-3"
+    },
+    "Airbus A320" => {
+        max_capacity: 162,
+        seat_arrangement: "3-3"
+    },
+    "Airbus A321" => {
+        max_capacity: 192,
+        seat_arrangement: "3-3"
+    },
+    "Airbus A330-200" => {
+        max_capacity: 232,
+        seat_arrangement: "2-4-2"
+    },
+    "Airbus A330-300" => {
+        max_capacity: 296,
+        seat_arrangement: "2-4-2"
+    },
+    "Airbus A350-900" => {
+        max_capacity: 315,
+        seat_arrangement: "3-3-3"
+    },
+    "Airbus A350-1000" => {
+        max_capacity: 369,
+        seat_arrangement: "3-3-3"
+    },
+    "Boeing 737-700" => {
+        max_capacity: 144,
+        seat_arrangement: "3-3"
+    },
+    "Boeing 737-8 MAX" => {
+        max_capacity: 174,
+        seat_arrangement: "3-3"
+    },
+    "Boeing 737-800" => {
+        max_capacity: 162,
+        seat_arrangement: "3-3"
+    },
+    "Boeing 737-900" => {
+        max_capacity: 180,
+        seat_arrangement: "3-3"
+    },
+    "Boeing 757-200" => {
+        max_capacity: 198,
+        seat_arrangement: "3-3"
+    },
+    "Boeing 757-300" => {
+        max_capacity: 234,
+        seat_arrangement: "3-3"
+    },
+    "Boeing 767-300" => {
+        max_capacity: 266,
+        seat_arrangement: "2-3-2"
+    },
+    "Boeing 767-400" => {
+        max_capacity: 308,
+        seat_arrangement: "2-3-2"
+    },
+    "Boeing 777-200" => {
+        max_capacity: 315,
+        seat_arrangement: "3-3-3"
+    },
+    "Boeing 777-300" => {
+        max_capacity: 396,
+        seat_arrangement: "3-3-3"
+    },
+    "Bombardier CRJ-200" => {
+        max_capacity: 48,
+        seat_arrangement: "2-2"
+    },
+    "Bombardier CRJ-700" => {
+        max_capacity: 72,
+        seat_arrangement: "2-2"
+    },
+    "Bombardier CRJ-900" => {
+        max_capacity: 80,
+        seat_arrangement: "2-2"
+    },
+    "Bombardier CRJ-1000" => {
+        max_capacity: 104,
+        seat_arrangement: "2-2"
+    },
+    "Embraer 170" => {
+        max_capacity: 72,
+        seat_arrangement: "2-2"
+    },
+    "Embraer 175" => {
+        max_capacity: 80,
+        seat_arrangement: "2-2"
+    },
+    "Embraer 190" => {
+        max_capacity: 100,
+        seat_arrangement: "2-2"
+    },
+    "Embraer 195" => {
+        max_capacity: 116,
+        seat_arrangement: "2-2"
+    }
+}
